00f67308d81a977155a63afdc0c7e4b4888fb894
Kaveesha420 <icetkaveesha@gmail.com>
2025-12-02 22:49:57 +0530

    Completed Product Entity

--- src/main/java/edu/icet/shop/model/Entity/Product.java
+++ src/main/java/edu/icet/shop/model/Entity/Product.java
@@ -1,4 +1,18 @@
 package edu.icet.shop.model.Entity;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private int qty;
+    private Double unitPrice;
 }
