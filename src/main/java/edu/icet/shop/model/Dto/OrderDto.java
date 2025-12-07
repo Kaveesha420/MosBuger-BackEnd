e08f57fd40469cad46e0c40bc01195c8dc9e6e44
Kaveesha420 <icetkaveesha@gmail.com>
2025-12-07 23:44:24 +0530

    Created OrderDto

--- src/main/java/edu/icet/shop/model/Dto/OrderDto.java
+++ src/main/java/edu/icet/shop/model/Dto/OrderDto.java
@@ -1,4 +1,20 @@
 package edu.icet.shop.model.Dto;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class OrderDto {
+    private String orderId;
+    private String orderDetailsId;
+    private String customerId;
+    private String productId;
+    private int qty;
+    private LocalDate localDate;
+    private Double totalPrice;
 }
